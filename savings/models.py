f27390c5c2972f97055bf5f08e200a08259ee5a1
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-01 13:44:02 +0000

    Create School model. Enable in admin.

--- savings/models.py
+++ savings/models.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(_('name'), max_length=50)
+    address = models.TextField(_('address'))
+    name_of_head = models.CharField(_('name of school head'), max_length=50)
+    phone_number = models.CharField(_('phone number'), max_length=11)
+    email = models.EmailField(_('email'))
+    password = models.CharField(_('password'), max_length=128)
+
+    def __str__(self):
+        return self.name
+
+""" class Parent(models.Model):
+    pass
+
+class Agent(models.Model):
+    pass """
