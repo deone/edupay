4125b946103d01f3188699e016abd3be7d9a1e1b
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-01 14:06:05 +0000

    School creation view.

--- edupay/urls.py
+++ edupay/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from savings import views
+
 urlpatterns = [
+    url(r'^school/new', views.SchoolCreate.as_view()),
     url(r'^admin/', admin.site.urls),
 ]
--- savings/views.py
+++ savings/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from models import School
+
+class SchoolCreate(CreateView):
+    model = School
+    fields = ['name', 'address', 'name_of_head', 'phone_number', 'email', 'password']
