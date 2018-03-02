37f124034f3316a803a9a3fcf959067022d85e5f
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-02 12:17:18 +0000

    Change field type.

--- savings/models.py
+++ savings/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class School(models.Model):
     name = models.CharField(_('name'), max_length=50)
-    address = models.TextField(_('address'))
+    address = models.CharField(_('address'), max_length=255)
     name_of_head = models.CharField(_('name of school head'), max_length=50)
     phone_number = models.CharField(_('phone number'), max_length=11)
     email = models.EmailField(_('email'))
