8779c1197068ed4cc8000351c3d1c541f37b47c4
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-02 13:26:09 +0000

    Display remaining fields.

--- savings/forms.py
+++ savings/forms.py
@@ -7,5 +7,27 @@ class SchoolForm(forms.ModelForm):
         model = School
         fields = '__all__'
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'St. Alexis International School', 'class': 'form-control'})
+            'name': forms.TextInput(attrs={
+                'placeholder': 'St. Alexis International School',
+                'class': 'form-control'
+            }),
+            'address': forms.TextInput(attrs={
+                'placeholder': '12 Main St, Ebute-Metta',
+                'class': 'form-control'
+            }),
+            'name_of_head': forms.TextInput(attrs={
+                'placeholder': 'Seyi Chukwuka',
+                'class': 'form-control'
+            }),
+            'phone_number': forms.NumberInput(attrs={
+                'placeholder': '07033445566',
+                'class': 'form-control'
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'you@example.com',
+                'class': 'form-control'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-control'
+            })
         }
