fbaa709ab2afd3928f7e2d9943fe11e203d89a18
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-03 10:54:54 +0000

    Add user field, remove email and password fields.

--- savings/models.py
+++ savings/models.py
@@ -2,15 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 class School(models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(_('name'), max_length=50)
     address = models.CharField(_('address'), max_length=255)
     name_of_head = models.CharField(_('name of school head'), max_length=50)
     phone_number = models.CharField(_('phone number'), max_length=11)
-    email = models.EmailField(_('email'))
-    password = models.CharField(_('password'), max_length=128)
 
     def __str__(self):
         return self.name
