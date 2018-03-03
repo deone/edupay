694dc1c8ac2cad18078691b027e5358cf2512906
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-03 14:10:12 +0000

    Increase field max length.

--- savings/models.py
+++ savings/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class School(models.Model):
     user = models.ForeignKey(User)
-    name = models.CharField(_('name'), max_length=50)
+    name = models.CharField(_('name'), max_length=100)
     address = models.CharField(_('address'), max_length=255)
     name_of_head = models.CharField(_('name of school head'), max_length=50)
     phone_number = models.CharField(_('phone number'), max_length=11)
