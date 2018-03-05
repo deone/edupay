0a5003ff14ad56a06145f5ee1c3ff5dcc32b209f
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-05 22:28:43 +0000

    Display form label tag in template.

--- savings/forms.py
+++ savings/forms.py
@@ -1,33 +1,40 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from models import School
 
-class SchoolForm(forms.ModelForm):
-    class Meta:
-        model = School
-        fields = '__all__'
-        widgets = {
-            'name': forms.TextInput(attrs={
+class SchoolForm(forms.Form):
+    name = forms.CharField(label=_('School name'), max_length=50, widget=forms.TextInput(attrs={
                 'placeholder': 'St. Alexis International School',
                 'class': 'form-control'
-            }),
-            'address': forms.TextInput(attrs={
+            }))
+    address = forms.CharField(label=_('Address'), max_length=255, widget=forms.TextInput(attrs={
                 'placeholder': '12 Main St, Ebute-Metta',
                 'class': 'form-control'
-            }),
-            'name_of_head': forms.TextInput(attrs={
+            }))
+    name_of_head = forms.CharField(label=_('Name of school head'), max_length=50, widget=forms.TextInput(attrs={
                 'placeholder': 'Seyi Chukwuka',
                 'class': 'form-control'
-            }),
-            'phone_number': forms.NumberInput(attrs={
+            }))
+    phone_number = forms.IntegerField(label=_('Phone number'), widget=forms.NumberInput(attrs={
                 'placeholder': '07033445566',
                 'class': 'form-control'
-            }),
-            'email': forms.EmailInput(attrs={
+            }))
+    email = forms.EmailField(label=_('Email'), widget=forms.EmailInput(attrs={
                 'placeholder': 'you@example.com',
                 'class': 'form-control'
-            }),
-            'password': forms.PasswordInput(attrs={
+            }))
+    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput(attrs={
                 'class': 'form-control'
-            })
-        }
+            }))
+
+    def save(self):
+        data = self.cleaned_data
+        email, password = data['email'], data['password']
+        phone_number, name_of_head = '0' + str(data['phone_number']), data['name_of_head']
+        name, address = data['name'], data['address']
+
+        user = User.objects.create_user(email, email, password)
+        school = School.objects.create(
+            user=user, phone_number=phone_number, name_of_head=name_of_head, address=address, name=name)
