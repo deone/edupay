08d1b9720a45110e4dce7d74ca4666c29d20ed9f
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-05 22:28:04 +0000

    Change SchoolCreate to regular view.

--- savings/views.py
+++ savings/views.py
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 
 from forms import SchoolForm
+from models import Agent
 
-class SchoolCreate(CreateView):
-    form_class = SchoolForm
-    template_name = 'savings/school_form.html'
-    success_url = '/school/new'
+def create_school(request):
+    if request.method == 'POST':
+        form = SchoolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/school/new')
+    else:
+        form = SchoolForm(label_suffix='')
+    return render(request, 'savings/school_form.html', {'form': form})
+
+# def create_parent(request):
+    # return render(request, 'savings/parent_form.html', {'form': form})
+
+class AgentCreate(CreateView):
+    model = Agent
+    fields = '__all__'
+    success_url = '/agent/new'
