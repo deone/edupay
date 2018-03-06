a684d8364b025937c31ca21c9772b80f3ecc542f
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-03-06 14:39:18 +0000

    New app accounts. Login template.

--- edupay/urls.py
+++ edupay/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from savings import views
+from accounts import views as accounts_views
 
 urlpatterns = [
+    url(r'^$', accounts_views.login),
     url(r'^school/new', views.create_school),
     url(r'^agent/new', views.AgentCreate.as_view()),
     # url(r'^parent/new', views.create_parent),
