059490ab0092e5da8d90c918d738cf9252449348
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-04-03 12:25:44 +0000

    Enable Parent model in admin. Display fields.

--- savings/admin.py
+++ savings/admin.py
@@ -3,7 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import School, Agent
+from models import School, Agent, Parent
+
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'house_address', 'work_address')
 
 admin.site.register(School)
 admin.site.register(Agent)
+admin.site.register(Parent, ParentAdmin)
