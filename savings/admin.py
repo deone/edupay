3a1302485a3989e6fd153ccd82a5f5f33e0ad40c
Oladayo Osikoya <alwaysdeone@gmail.com>
2018-04-04 18:24:27 +0000

    Display fields in admin.

--- savings/admin.py
+++ savings/admin.py
@@ -9,13 +9,19 @@ class ParentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'house_address', 'work_address')
 
 class SavingPlanAdmin(admin.ModelAdmin):
-    list_display = ('parent_name', 'amount_to_be_saved', 'target_date', 'contribution')
+    list_display = ('parent_name', 'amount_to_be_saved', 'target_date', 'contribution', 'date_created')
 
     def parent_name(self, obj):
         return obj.parent.get_full_name()
 
-admin.site.register(School)
+class SavingAdmin(admin.ModelAdmin):
+    list_display = ('saving_plan_parent_name', 'amount', 'date_created')
+
+    def saving_plan_parent_name(self, obj):
+        return obj.saving_plan.parent.get_full_name()
+
 admin.site.register(Agent)
-admin.site.register(SavingPlan, SavingPlanAdmin)
-admin.site.register(Saving)
+admin.site.register(School)
+admin.site.register(Saving, SavingAdmin)
 admin.site.register(Parent, ParentAdmin)
+admin.site.register(SavingPlan, SavingPlanAdmin)
